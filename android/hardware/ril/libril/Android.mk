4ba2a8f787535006d6d8074cb690cc5d6cebef64
August <mingxin.android@gmail.com>
2015-11-15 02:51:08 +0800

    upload android source part7

---
+++ android/hardware/ril/libril/Android.mk
@@ -0,0 +1,49 @@
+# Copyright 2006 The Android Open Source Project
+
+LOCAL_PATH:= $(call my-dir)
+
+# bpi, prebuild lib used if have simcom modem
+ifeq ($(TARGET_SIMCOM_USED),true)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+    ril.cpp \
+    ril_event.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libutils \
+    libbinder \
+    libcutils \
+    libhardware_legacy
+
+LOCAL_CFLAGS :=
+
+LOCAL_MODULE:= libril
+
+LOCAL_LDLIBS += -lpthread
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif
+
+# For RdoServD which needs a static library
+# =========================================
+ifneq ($(ANDROID_BIONIC_TRANSITION),)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+    ril.cpp
+
+LOCAL_STATIC_LIBRARIES := \
+    libutils_static \
+    libcutils
+
+LOCAL_CFLAGS :=
+
+LOCAL_MODULE:= libril_static
+
+LOCAL_LDLIBS += -lpthread
+
+include $(BUILD_STATIC_LIBRARY)
+endif # ANDROID_BIONIC_TRANSITION
